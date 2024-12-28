d9ac636e859f3775d05e6960aad2dd34220df7e7
Dem4k <demch1kdanya@gmail.com>
2024-12-28 16:02:22 +0200

    third

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,39 @@
+// // src/components/Product.jsx
+
+// export default function Product({
+//   name,
+//   imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+//   price,
+// }) {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <img src={imgUrl} alt={name} width="480" />
+//       <p>Price: {price} credits</p>
+//     </div>
+//   );
+// }
+
 // src/components/Product.jsx
 
-export default function Product(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img
-        src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        alt="Tacos With Lime"
-        width="480"
-      />
-      <p>Price: 999 credits</p>
-    </div>
-  );
-}
+import PropTypes from "prop-types";
+
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+    <img src={imgUrl} alt={name} width="480" />
+    <h2>{name}</h2> <p>Price: {price} credits</p>{" "}
+  </div>
+);
+
+// Опис типів пропсів компонента
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
+};
+
+export default Product;
